dee16f8023495c0bbca44d86ac506b3de8bed9bd
ZetKamil <kamil.ziemkiewicz.97@gmail.com>
2026-04-27 10:58:31 +0200

    migrations

---
+++ database/migrations/2026_04_27_085330_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
+            $table->string('order_number')->unique()->index();
+            $table->decimal('total_amount', 10, 2);
+            $table->string('status')->index(); // Linked to OrderStatus enum
+            $table->string('stripe_payment_intent_id')->nullable();
+            $table->string('stripe_session_id')->nullable();
+            $table->json('address_details');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
